8b7603bd32022b1f36fdad4eb21624b2162d507f
Kickassdud3 <ajmills2004@gmail.com>
2016-05-02 12:46:24 +0100

    1.0
    added javascript for menu button

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+//add all functions which need to run here
+  navIconClick();
+
+  function navIconClick {
+    $navIcon = $('#nav-icon');
+    $navIcon.click(function() {
+      
+    });
+  });
+
+// add all functions here
+  });
